61f3a133f1a76e93021435edc66d69393f806567
conor-wilson <conorwilson1998@gmail.com>
2024-10-05 21:00:03 +1000

    Added flicker shader to enemies when they get damaged

--- shrinker/shrinker/scenes/entities/bee.gd
+++ shrinker/shrinker/scenes/entities/bee.gd
@@ -1,5 +1,19 @@
 extends Area2D
 
+# TODO: This de-duplicate the enemy code
+
+var health := 3
+
+func _process(delta: float) -> void:
+	check_death()
+
+func check_death(): 
+	if health == 0:
+		queue_free()
+
 func _on_area_entered(area: Area2D) -> void:
-	print("bee was hit!")
+	health -= 1
 	area.queue_free()
+	var tween = create_tween()
+	tween.tween_property($Sprite2D, "material:shader_parameter/amount", 1.0, 0.0)
+	tween.tween_property($Sprite2D, "material:shader_parameter/amount", 0.0, 0.1).set_delay(0.2)
--- shrinker/shrinker/scenes/entities/worm.gd
+++ shrinker/shrinker/scenes/entities/worm.gd
@@ -1,5 +1,17 @@
 extends Area2D
 
+var health := 2
+
+func _process(delta: float) -> void:
+	check_death()
+
+func check_death(): 
+	if health == 0:
+		queue_free()
+
 func _on_area_entered(area: Area2D) -> void:
-	print("worm was hit!")
+	health -= 1
 	area.queue_free()
+	var tween = create_tween()
+	tween.tween_property($Sprite2D, "material:shader_parameter/amount", 1.0, 0.0)
+	tween.tween_property($Sprite2D, "material:shader_parameter/amount", 0.0, 0.1).set_delay(0.2)
