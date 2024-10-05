b281d615531009b4b56e58d66e0341ac063068a4
conor-wilson <conorwilson1998@gmail.com>
2024-10-05 17:13:20 +1000

    Initialised bullet firing

--- shrinker/shrinker/scenes/entities/player.gd
+++ shrinker/shrinker/scenes/entities/player.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
+signal fire_bullet
+
 @export var walk_speed         := 300.0
 @export var jump_speed         := 600.0
 @export var gravity_multiplier := 1.0
 @export var glide_effect       := 0.5
+@export var bullet_scene       : PackedScene
 
-func _physics_process(delta: float) -> void:
+var has_gun : bool = true
+
+func _process(delta: float) -> void:
 	apply_gravity(delta)
 	handle_input()
+	set_animation()
 	move_and_slide()
 
 func apply_gravity(delta: float):
@@ -19,19 +25,26 @@ func apply_gravity(delta: float):
 		
 		velocity += vel_due_to_gravity
 
-func handle_input(): 
+func handle_input():
+	
+	# Handle jump input
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		jump()
+	
+	# Handle shoot input
 	if Input.is_action_just_pressed("Shoot") and $ShootCooldown.is_stopped():
 		shoot()
+	
+	# Handle the directional input
 	var direction := Input.get_axis("Left", "Right")
 	move(direction)
+	set_direction(direction)
 
 func jump():
 	velocity.y = -jump_speed
 
 func shoot():
-	print("SHOOT!")
+	fire_bullet.emit()
 	$ShootCooldown.start()
 
 func move(direction:float):
@@ -39,3 +52,30 @@ func move(direction:float):
 		velocity.x = direction * walk_speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, walk_speed)
+
+func set_animation():
+	
+	# Idle animation
+	if velocity == Vector2.ZERO:
+		if has_gun:
+			$AnimatedSprite2D.animation = 'idle_gun'
+		else: 
+			$AnimatedSprite2D.animation = 'idle'
+	
+	# Jump animation
+	elif !is_on_floor():
+		if has_gun:
+			$AnimatedSprite2D.animation = 'jump_gun'
+		else: 
+			$AnimatedSprite2D.animation = 'jump'
+	
+	# Walk animation
+	else:
+		if has_gun:
+			$AnimatedSprite2D.animation = 'walk_gun'
+		else: 
+			$AnimatedSprite2D.animation = 'walk'
+
+func set_direction(direction:float):
+	if direction != 0:
+		$AnimatedSprite2D.flip_h = (direction <= 0)
---
+++ shrinker/shrinker/scenes/levels/level.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_player_fire_bullet() -> void:
+	var bullet : Node = $Player.bullet_scene.instantiate()
+	add_child(bullet)
+	bullet.global_position = $Player.global_position
