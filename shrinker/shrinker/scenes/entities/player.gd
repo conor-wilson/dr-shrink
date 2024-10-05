ca782d908069241456fd2e238c452b1c80a9ded9
conor-wilson <conorwilson1998@gmail.com>
2024-10-05 11:28:37 +1000

    Made player's physics configurable via the main scene

--- shrinker/shrinker/scenes/entities/player.gd
+++ shrinker/shrinker/scenes/entities/player.gd
@@ -1,25 +1,25 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
-
+@export var walk_speed         : float = 300.0
+@export var jump_speed         : float = 600.0
+@export var gravity_multiplier : float = 1.0
 
 func _physics_process(delta: float) -> void:
+	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * gravity_multiplier * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = -jump_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("Left", "Right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * walk_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, walk_speed)
 
 	move_and_slide()
